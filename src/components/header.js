62f15282ca9f5e8b04670a9d20faa051212725f0
Tony Porter <tonyporter89@gmail.com>
2020-02-03 20:03:34 +0000

    installed react-icons

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import styled from "styled-components"
-import Media from "react-media"
+import MediaQuery from "react-media"
 import HeaderLogo from '../images/headerLogo.png'
 import DesktopNav from './nav/desktopNav'
 import ResponsiveNav from './nav/responsiveNav'
@@ -39,9 +39,9 @@ const Header = () => (
         <img src={HeaderLogo} alt="Logo" />
       </HeaderLogoLink>
       <Nav>
-        <Media query="(max-width: 880px)">
+        <MediaQuery query="(max-width: 880px)">
           {matches => (matches ? <ResponsiveNav /> : <DesktopNav />)}
-        </Media>
+        </MediaQuery>
       </Nav>
     </Inner>
   </Container>
